3401c5d1203c2a85f023415f4cb2d609da0bfbf9
simomu-github <simomutter@gmail.com>
2020-06-05 12:15:37 +0900

    add sample sale date to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+this_year_sale_values = [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+this_year_sale_values.each_with_index do |sale_value, index|
+  Sale.create(date: Time.current.beginning_of_year + index.month, sale_value: sale_value)
+end
+
+last_year_sale_values = [700, 1700, 2700, 2000, 1800, 1500, 2000]
+last_year_sale_values.each_with_index do |sale_value, index|
+  Sale.create(date: Time.current.years_ago(1).beginning_of_year + index.month, sale_value: sale_value)
+end
+
